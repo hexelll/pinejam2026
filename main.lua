426972d5f7645c7ac4a2e32aff23ef0d391f4522
TheoALBERT <theo.albert.6464@gmail.com>
2026-08-06 19:51:46 +0200

    feat(main) start of game logic

--- main.lua
+++ main.lua
@@ -15,6 +15,19 @@ local Color = import 'Color.lua'
 local MakeImage = import "pipelines/GenerateImage.lua"
 local char,square = import('combinators/MathCharCombinator.lua'):new(),import('combinators/SquarePixelCombinator.lua'):new()
 
+
+--[[
+    INIT GAME
+]]
+write("Builing the map... ")
+local map = MapGenerator.makeMap()
+print("Map ready")
+
+
+--[[
+    SETUP DISPLAY
+]]
+write("Initializing display... ")
 local mon = peripheral.find('monitor')
 mon.setTextScale(0.5)
 
@@ -23,14 +36,74 @@ local screen = Renderer:new{
     combinators={char,square}
 }
 
-print("making map")
-local map = MapGenerator.makeMap()
-
-print("making pipeline")
-sleep()
+gameHandler.state = {
+    biomeMap = map.biomeMap,
+}
 local pipeline = mapExplorator(gameHandler,screen, map.prettyMap)
-    --:after("zoom","maskEdge")
+print("Display ready")
 
-print("yay")
 
-gameHandler:run{}
+--[[
+    GAME LOGIC 
+]]
+-- bombs logic
+gameHandler:addTask(function (state)
+    --[[
+    local delayBombs = (1/state.dangerLevel)*5
+
+    if ( os.clock() - state.bombWarningTime > delayBombs )then
+   
+        -- add bombsWarning
+        bombsWarnings[#bombsWarnings+1] = {
+            x,
+            y,
+            sx,
+            sy,
+            bombingTime = os.clock()+10
+        }
+
+        print("BOMBS COMMING")
+
+        state.bombWarningTime = os.clock()
+    end
+    ]]
+end)
+
+-- days logic
+gameHandler:addTask(function (state)
+    if ( os.clock() - state.dayChangeTime > 5 )then -- time between days
+
+        if (state.isAlive) then
+            state.ressources = state.ressources + state.dayNb
+            
+            print("You have ".. state.ressources .." ressources")
+        end
+
+        state.dangerLevel = state.dangerLevel*1.1
+        state.dayNb = state.dayNb + 1
+        state.dayChangeTime = os.clock()
+    end
+end)
+
+-- end logic
+gameHandler:addTask(function (state)
+    if state.cities and #state.cities <= 0 then
+        state.isAlive = false
+        print("GAME OVER !")
+    end
+end)
+
+
+--[[
+    START 
+]]
+print("Game started !")
+gameHandler:run{
+    isAlive = true,
+    dangerLevel = 1,
+    ressources = 4,
+    dayNb = 1,
+    dayChangeTime = os.clock(),
+    bombWarningTime = os.clock(),
+    bombsWarnings = {}
+}
